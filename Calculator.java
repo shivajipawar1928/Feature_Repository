64119f8756b34646cc0a10d9b20b61ba3d0223c6
ShivajiPawar <shivajipawar1928@gmail.com>
2023-11-01 20:25:10 +0530

    Calculator added

---
+++ Calculator.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the first number: ");
+        double num1 = scanner.nextDouble();
+
+        System.out.println("Enter the second number: ");
+        double num2 = scanner.nextDouble();
+
+        // Calculate the sum, difference, product, and quotient
+        double sum = num1 + num2;
+        double difference = num1 - num2;
+        double product = num1 * num2;
+        double quotient = num1 / num2;
+
+        // Display the results
+        System.out.println("Sum: " + sum);
+        System.out.println("Difference: " + difference);
+        System.out.println("Product: " + product);
+        System.out.println("Quotient: " + quotient);
+
+        scanner.close();
+    }
+}
